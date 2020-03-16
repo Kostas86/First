53aed410361bf33da9e46c54733b2ce5dec44905
kostas86 <serebrennikovnumberone@gmail.com>
2020-03-16 22:23:37 +0300

    16.03 22:23

---
+++ project/js/script.js
@@ -0,0 +1,9 @@
+
+        ymaps.ready(function(){
+            var map = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 10
+            });
+            
+        });
+  
